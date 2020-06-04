f1d14b584e50f17fbcbb41a183e59b1956c072aa
2623992243 <2623992243@qq.com>
2020-06-04 08:55:01 +0800

    购物车案例

--- 08-购物车案例/main.js
+++ 08-购物车案例/main.js
@@ -41,11 +41,56 @@ const app = new Vue({
     },
     computed : {
         totalPrice(){
-            let price = 0
-            for(let i = 0;i<this.books.length;i++){
-                price += this.books[i].price * this.books[i].count
-            }
-            return price
+            // let price = 0
+            // for(let i = 0;i<this.books.length;i++){
+            //     price += this.books[i].price * this.books[i].count
+            // }
+            
+            // 2、for(let i in this.books)
+            // for(let i in this.books){
+            //     price += this.books[i].price * this.books[i].count
+            // }
+
+            // 3、for(let item of this.books)
+            // for(let item of this.books){
+            //     price += item.price * item.count
+            // }
+            // return price
+            
+            return this.books.reduce(function(preValue,book){
+                return preValue + book.price * book.count
+            },0)
         }
     }
 })
+
+const nums = [10,430,20,60,213,543,10]
+
+// 1、filter函数的使用
+// filter中的回调函数有一个要求，必须返回一个boolean值
+// true：当返回true时，函数内部会自动将这次回调的n加入到新的数组中
+// false：当返回false时，函数内部会自动过滤这次的n
+// let newNums = nums.filter(function(n){
+//     return n < 100
+// })
+
+// // 2、map函数的使用
+// let new2Nums =  newNums.map(function(n){
+//     return n * 2
+// })
+
+// let totalPrice = new2Nums.reduce(function(preValue,n){
+//     return preValue + n
+// })
+
+// let count = nums.filter(function(n){
+//     return n < 100
+// }).map(function(n){
+//     return n * 2
+// }).reduce(function(preValue,n){
+//     return preValue + n
+// })
+
+let count = nums.filter(n => n < 100).map(n => n * 2).reduce((preValue,n) => preValue + n)
+
+console.log(count)
